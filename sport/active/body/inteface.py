e205fe782a52cc2537a4898aff72bc51e62551ce
朱林忠 <wb_zhulinzhong@meituan.com>
2022-04-21 19:51:14 +0800

    init

---
+++ sport/active/body/inteface.py
@@ -0,0 +1,15 @@
+class Body:
+
+    
+
+    def reset(self):
+        print('reset')
+
+    def move_x(self, angle: int, step=1):
+        print('move-x')
+
+    def move_y(self, angle: int, step=1):
+        print('move-y')
+
+    def move_z(self, angle: int, step=1):
+        print('move-z')
---
+++ sport/basic/SteeringEngine.py
@@ -0,0 +1,7 @@
+class SteeringEngine:
+
+    angle = 0
+
+    def to(self, angle: int):
+        self.angle = angle
+        print('to')
